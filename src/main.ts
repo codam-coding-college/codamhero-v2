aac562d1b44f1255d52b23fafaf61691e2ec0826
Freek Bes <freek.bes@codam.nl>
2024-06-04 18:28:50 +0200

    add first draft of piscine overview

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,56 @@ dotenv.config({ path: '.env', debug: true });
 import express from 'express';
 import nunjucks from 'nunjucks';
 
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Prisma } from "@prisma/client";
 const prisma = new PrismaClient();
 
 import Fast42 from '@codam/fast42';
 import { syncWithIntra } from './intra/base';
-let firstSyncComplete = false;
+import { getAllPiscines } from './utils';
+let firstSyncComplete = true;
 
 const INTRA_API_UID = process.env.INTRA_API_UID!;
 const INTRA_API_SECRET = process.env.INTRA_API_SECRET!;
 
 const app = express();
 
-nunjucks.configure('templates', {
+const nunjucksEnv = nunjucks.configure('templates', {
 	autoescape: true,
 	express: app,
+});
+
+// Add formatting filter for seconds to hh:mm format
+nunjucksEnv.addFilter('formatSeconds', (seconds: number) => {
+	const hours = Math.floor(seconds / 3600);
+	const minutes = Math.floor((seconds % 3600) / 60);
+	return `${hours.toString().padStart(2, '0')}h${minutes.toString().padStart(2, '0')}`;
+});
+
+// Add formatting filter to format a date as "... minutes/hours/days ago"
+nunjucksEnv.addFilter('timeAgo', (date: Date | null) => {
+	if (!date) {
+		return 'never';
+	}
+
+	const now = new Date();
+	const diff = now.getTime() - date.getTime();
+	const seconds = Math.floor(diff / 1000);
+	const minutes = Math.floor(seconds / 60);
+	const hours = Math.floor(minutes / 60);
+	const days = Math.floor(hours / 24);
+
+	if (days > 2) { // < 3 days we want to see the hours
+		return `${days} days ago`;
+	}
+	else if (hours > 1) {
+		return `${hours} hours ago`;
+	}
+	else if (minutes > 15) { // > 15 because we synchronize every 15 minutes, otherwise we'll show "just now"
+		return `${minutes} minutes ago`;
+	}
+	else {
+		return `just now`;
+	}
 });
 
 const waitForFirstSync = async function(req: express.Request, res: express.Response, next: express.NextFunction) {
@@ -101,38 +136,14 @@ app.get('/users/pisciners', async (req, res) => {
 	}
 });
 
-// Make sure the year starts with 20 and the month is between 01 and 12
+// TODO: Make sure the year starts with 20 and the month is between 01 and 12
 app.get('/users/pisciners/:year/:month', async (req, res) => {
 	// Parse parameters
 	const year = parseInt(req.params.year);
 	const month = parseInt(req.params.month);
 
-	// Find all possible piscines
-	const piscines_users = await prisma.user.findMany({
-		select: {
-			pool_year: true,
-			pool_year_num: true,
-			pool_month: true,
-			pool_month_num: true,
-		},
-		distinct: ['pool_year', 'pool_month'],
-		orderBy: [
-			{ pool_year_num: 'desc' },
-			{ pool_month_num: 'desc' },
-		],
-	});
-	const piscines = piscines_users.flatMap((p) => {
-		// Do not include empty pool_month or pool_year
-		if (!p.pool_year || !p.pool_month || !p.pool_year_num || !p.pool_month_num || p.pool_year_num < 1 || p.pool_month_num < 1) {
-			return [];
-		}
-		return {
-			year: p.pool_year,
-			year_num: p.pool_year_num,
-			month: p.pool_month,
-			month_num: p.pool_month_num,
-		};
-	});
+	// Find all possible piscines from the database
+	const piscines = await getAllPiscines(prisma);
 
 	// Find all users for the given year and month
 	const users = await prisma.user.findMany({
@@ -148,13 +159,123 @@ app.get('/users/pisciners/:year/:month', async (req, res) => {
 				not: "admin",
 			},
 		},
-		orderBy: [
-			{ first_name: 'asc' },
-			{ last_name: 'asc' },
-		],
+		orderBy: {
+			first_name: 'asc',
+			last_name: 'asc',
+		},
 	});
 
 	return res.render('users.njk', { piscines, users, year, month });
+});
+
+app.get('/piscines', async (req, res) => {
+	// Redirect to latest year and month defined in the database
+	const latest = await prisma.user.findFirst({
+		orderBy: [
+			{ pool_year_num: 'desc' },
+			{ pool_month_num: 'desc' },
+		],
+	});
+	if (latest) {
+		return res.redirect(`/piscines/${latest.pool_year_num}/${latest.pool_month_num}`);
+	}
+	else {
+		// No pisciners found, return 404
+		res.status(404);
+		return;
+	}
+});
+
+interface PiscineLogTimes {
+	weekOne: number;
+	weekTwo: number;
+	weekThree: number;
+	weekFour: number;
+	total: number;
+};
+
+app.get('/piscines/:year/:month', async (req, res) => {
+	// Parse parameters
+	const year = parseInt(req.params.year);
+	const month = parseInt(req.params.month);
+
+	// Find all possible piscines from the database
+	const piscines = await getAllPiscines(prisma);
+
+	// Find all users for the given year and month
+	const users = await prisma.user.findMany({
+		where: {
+			pool_year_num: year,
+			pool_month_num: month,
+			kind: {
+				not: "admin",
+			},
+			cursus_users: {
+				some: {
+					cursus_id: 9,
+				},
+			}
+		},
+		include: {
+			project_users: true,
+			cursus_users: {
+				where: {
+					cursus_id: 9,
+				},
+			},
+			locations: {
+				// Only the latest or current one
+				take: 1,
+				where: {
+					primary: true,
+				},
+				orderBy: {
+					begin_at: 'desc',
+				},
+			},
+		},
+	});
+
+	// Sort users by level of their cursus
+	users.sort((a, b) => {
+		const aLevel = a.cursus_users[0]?.level || 0;
+		const bLevel = b.cursus_users[0]?.level || 0;
+		return bLevel - aLevel;
+	});
+
+	// Get logtime for each week of the piscine for each user
+	let logtimes: { [login: string]: PiscineLogTimes } = {}
+	for (const user of users) {
+		const piscineBegin = user.cursus_users[0]?.begin_at;
+		const weekTwo = new Date(piscineBegin.getTime() + 60 * 60 * 24 * 7 * 1000);
+		const weekThree = new Date(piscineBegin.getTime() + 60 * 60 * 24 * 14 * 1000);
+		const weekFour = new Date(piscineBegin.getTime() + 60 * 60 * 24 * 21 * 1000);
+		const piscineEnd = new Date(piscineBegin.getTime() + 60 * 60 * 24 * 25 * 1000);
+
+		const locationsDuringPiscine = await prisma.location.findMany({
+			where: {
+				user_id: user.id,
+				begin_at: {
+					gte: piscineBegin,
+					lte: piscineEnd,
+				},
+			},
+			orderBy: {
+				begin_at: 'asc',
+			},
+		});
+
+		// Calculate seconds spent in each week behind the computer
+		logtimes[user.login] = {
+			weekOne: locationsDuringPiscine.filter((l) => l.begin_at <= weekTwo).reduce((acc, l) => acc + (l.end_at!.getTime() - l.begin_at.getTime()) / 1000, 0),
+			weekTwo: locationsDuringPiscine.filter((l) => l.begin_at >= weekTwo && l.begin_at <= weekThree).reduce((acc, l) => acc + (l.end_at!.getTime() - l.begin_at.getTime()) / 1000, 0),
+			weekThree: locationsDuringPiscine.filter((l) => l.begin_at >= weekThree && l.begin_at <= weekFour).reduce((acc, l) => acc + (l.end_at!.getTime() - l.begin_at.getTime()) / 1000, 0),
+			weekFour: locationsDuringPiscine.filter((l) => l.begin_at >= weekFour).reduce((acc, l) => acc + (l.end_at!.getTime() - l.begin_at.getTime()) / 1000, 0),
+			total: locationsDuringPiscine.reduce((acc, l) => acc + (l.end_at!.getTime() - l.begin_at.getTime()) / 1000, 0),
+		};
+	}
+
+	return res.render('piscines.njk', { piscines, users, logtimes, year, month });
 });
 
 app.listen(3000, async () => {
@@ -167,7 +288,7 @@ app.listen(3000, async () => {
 			client_secret: INTRA_API_SECRET,
 		}]).init();
 
-		await syncWithIntra(api);
+		// await syncWithIntra(api);
 		firstSyncComplete = true;
 	}
 	catch (err) {
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { PrismaClient } from "@prisma/client";
+
 export const monthToNumber = (month: string): number => {
 	const months = [
 		'january',
@@ -20,3 +22,40 @@ export const monthToNumber = (month: string): number => {
 
 	return months.indexOf(month.toLowerCase()) + 1;
 };
+
+export interface Piscine {
+	year: string;
+	year_num: number;
+	month: string;
+	month_num: number;
+};
+
+export const getAllPiscines = async function(prisma: PrismaClient): Promise<Piscine[]> {
+	// Find all possible piscines
+	const piscines_users = await prisma.user.findMany({
+		select: {
+			pool_year: true,
+			pool_year_num: true,
+			pool_month: true,
+			pool_month_num: true,
+		},
+		distinct: ['pool_year', 'pool_month'],
+		orderBy: [
+			{ pool_year_num: 'desc' },
+			{ pool_month_num: 'desc' },
+		],
+	});
+	const piscines: Piscine[] = piscines_users.flatMap((p) => {
+		// Do not include empty pool_month or pool_year
+		if (!p.pool_year || !p.pool_month || !p.pool_year_num || !p.pool_month_num || p.pool_year_num < 1 || p.pool_month_num < 1) {
+			return [];
+		}
+		return {
+			year: p.pool_year,
+			year_num: p.pool_year_num,
+			month: p.pool_month,
+			month_num: p.pool_month_num,
+		};
+	});
+	return piscines;
+}
