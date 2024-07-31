df1c9fef901d2391d24dd654f605d499ca4d8f31
Freek Bes <freek.bes@codam.nl>
2024-07-31 18:46:32 +0200

    abort fetch if one was already running

--- static/js/clustermaps/history.js
+++ static/js/clustermaps/history.js
@@ -1,6 +1,8 @@
 const dateTimeSelector = document.getElementById('history-datetime-selector');
 
 let oldLocationData = [];
+const controller = new AbortController();
+const signal = controller.signal;
 
 function fetchLocationsAtDateTime() {
 	if (initLoad) {
@@ -8,7 +10,7 @@ function fetchLocationsAtDateTime() {
 		initLoad = null;
 	}
 	const dateTime = dateTimeSelector.value;
-	const req = fetch(`/clustermap/locations/${dateTime}`);
+	const req = fetch(`/clustermap/locations/${dateTime}`, { signal });
 	req.then(res => res.json()).then(data => {
 		const updatedLocations = filterUpdatedLocations(oldLocationData, data);
 		console.log("Fetched location data", data, updatedLocations);
@@ -20,6 +22,8 @@ function fetchLocationsAtDateTime() {
 			console.log("Adding location", newLocation);
 			createLocation(newLocation);
 		}
+	}).catch(err => {
+		console.error("Failed to fetch location data", err);
 	});
 }
 
