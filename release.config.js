a8fa6fa6b0eb481b4ac15ade0aef030aa389a575
Freek Bes <freek.bes@codam.nl>
2024-10-08 16:03:56 +0200

    fix: update package.json version on new release

--- release.config.js
+++ release.config.js
@@ -11,5 +11,12 @@ module.exports = {
 			},
 		],
 		"@semantic-release/github",
+		[
+			"@semantic-release/git",
+			{
+				assets: ["package.json"],
+				message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+			}
+		]
 	]
 };
