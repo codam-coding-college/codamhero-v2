11b0378c3cb47d66de282c731fbeb2d95b9ae7b5
Freek Bes <freek.bes@codam.nl>
2026-04-29 18:01:01 +0200

    fix: fix database migration scripts not working on non-empty database

--- prisma/migrations/20260429132457_add_project_cursus_id/migration.sql
+++ prisma/migrations/20260429132457_add_project_cursus_id/migration.sql
@@ -1,17 +1,21 @@
 /*
   Warnings:
 
-  - Added the required column `cursus_id` to the `Project` table without a default value. This is not possible if the table is not empty. Thus the projects table gets emptied and a resync of all projects is required. This will be done automatically.
+  - Added the required column `cursus_id` to the `Project` table.
 
 */
--- Drop all projects
-DELETE FROM "Project";
-
 -- Force resync of all projects to update the new `cursus_id` field
 UPDATE "Synchronization" SET "last_synced_at" = NULL WHERE "kind" = 'projects';
 
 -- AlterTable
-ALTER TABLE "Project" ADD COLUMN     "cursus_id" INTEGER NOT NULL;
+ALTER TABLE "Project" ADD COLUMN     "cursus_id" INTEGER;
+
+-- Set default cursus_id for existing projects (if needed, otherwise it can be left as NULL)
+-- Using cursus_id 1 because the cursus has to exist in the database for the foreign key constraint later on
+UPDATE "Project" SET "cursus_id" = 1 WHERE "cursus_id" IS NULL;
+
+-- Require cursus_id
+ALTER TABLE "Project" ALTER COLUMN "cursus_id" SET NOT NULL;
 
 -- AddForeignKey
 ALTER TABLE "Project" ADD CONSTRAINT "Project_cursus_id_fkey" FOREIGN KEY ("cursus_id") REFERENCES "Cursus"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- prisma/migrations/20260429133305_add_project_difficulty/migration.sql
+++ prisma/migrations/20260429133305_add_project_difficulty/migration.sql
@@ -1,14 +1,18 @@
 /*
   Warnings:
 
-  - Added the required column `difficulty` to the `Project` table without a default value. This is not possible if the table is not empty. Thus the projects table gets emptied and a resync of all projects is required. This will be done automatically.
+  - Added the required column `difficulty` to the `Project` table.
 
 */
--- Drop all projects
-DELETE FROM "Project";
 
 -- Force resync of all projects to update the new `difficulty` field
 UPDATE "Synchronization" SET "last_synced_at" = NULL WHERE "kind" = 'projects';
 
 -- AlterTable
-ALTER TABLE "Project" ADD COLUMN     "difficulty" INTEGER NOT NULL;
+ALTER TABLE "Project" ADD COLUMN     "difficulty" INTEGER;
+
+-- Set default difficulty for existing projects (if needed, otherwise it can be left as NULL)
+UPDATE "Project" SET "difficulty" = 0 WHERE "difficulty" IS NULL;
+
+-- Require difficulty
+ALTER TABLE "Project" ALTER COLUMN "difficulty" SET NOT NULL;
